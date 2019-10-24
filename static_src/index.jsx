6f7e828cbb6c5b04647a556864504287dfc94257
Elizaveta <lizasvit@gmail.com>
2019-10-24 14:36:38 +0300

    hw1 messages with button part

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -2,15 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-let messages = ['Привет', 'Как дела?'];
+let messages = [];
+let currentMessage = "";
 
 const MessageComponent = (props) => <div>{props.text}</div>;
 
 const MessageField = (props) => {
-    return props.messages.map(message => <MessageComponent text={ message } />);
+    return props.messages.map(message => <MessageComponent text={message}/>);
+};
+
+const handleChange = (e) => {
+    currentMessage = e.target.value;
+};
+
+const handleClick = () => {
+    messages.push(currentMessage);
+    console.log(currentMessage);
+
+    ReactDOM.render(
+        <Render/>,
+        document.getElementById('root'),
+    );
+};
+
+const Render = () => {
+    return (
+        <div>
+            <div style={{display: 'flex'}}>Your message: &ensp;
+                <input type="text" name="fname" onChange={handleChange}/>
+                <input
+                    type="button" value="Send" onClick={handleClick}
+                />
+
+            </div>
+            <div>
+                <h3>Message history:</h3>
+                <MessageField messages={messages}/>
+            </div>
+        </div>
+    );
 };
 
 ReactDOM.render(
-    <MessageField messages={ messages } />,
+    <Render/>,
     document.getElementById('root'),
 );
+
