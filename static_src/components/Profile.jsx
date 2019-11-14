ad1cf69b094235fa0e9b9a0795af1cd6a607a8a6
Elizaveta <lizasvit@gmail.com>
2019-11-14 15:57:07 +0300

    style files separated

--- static_src/components/Profile.jsx
+++ static_src/components/Profile.jsx
@@ -1,7 +1,6 @@
 import Header from './Header'
 import React from "react";
 import PropTypes from "prop-types";
-import '../styles/styles.css';
 
 
 export default class Profile extends React.Component {
